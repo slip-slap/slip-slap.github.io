a1df46f76918585a572b620de71709ad486f3a9d
kismet <ZhangHY1012@gmail.com>
2020-02-29 10:56:25 +0900

    me

--- docs/bash.sh
+++ docs/bash.sh
@@ -1,13 +1,18 @@
-rm -rf docs
-mkdir docs
-cd docs
-cp ../CNAME ./
-cp -r ../_site/* ./
+git add .
+git commit -m "me"
+git push origin develop
 
-cd ..
+mkdir ~/Desktop/docs
+cp -r ./_site/* ~/Desktop/docs
 
+git checkout master
+
+cp -r ~/Desktop/docs ./docs
+rm -r  ~/Desktop/docs
 git add .
 git commit -m "me"
 git push origin master
-rm -rf docs
+
+git checkout develop
+
 
