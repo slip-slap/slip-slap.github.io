bfdb914ef97ff3d9b7c767a28a64c51eb2a489d8
liam <ZhangHY1012@gmail.com>
2020-11-25 21:11:06 +0900

    me

--- bash.sh
+++ bash.sh
@@ -2,13 +2,13 @@ git add .
 git commit -m "me"
 git push origin develop
 
-mkdir ~/Desktop/docs
-cp -r ./_site/* ~/Desktop/docs
+mkdir ~/Desktop/asdfasdiajlskdfajsdkfasdkjaksd
+cp -r ./_site/* ~/Desktop/asdfasdiajlskdfajsdkfasdkjaksd
 
 git checkout master
 
-cp -r  ~/Desktop/docs/* ./
-rm -r  ~/Desktop/docs
+cp -r  ~/Desktop/asdfasdiajlskdfajsdkfasdkjaksd/* ./
+rm -r  ~/Desktop/asdfasdiajlskdfajsdkfasdkjaksd
 git add .
 git commit -m "me"
 git push origin master
